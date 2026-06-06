c10eeedcf2a468a260a0cdc9d9ca305f5659e652
Joao Garcez <jvgarcez2001@gmail.com>
2026-06-05 23:53:39 -0300

    Ignorado arquivos listados no gitignore

--- teste.py
+++
@@ -1,7 +0,0 @@
-﻿from dao.usuario_dao import UsuarioDAO
-from models.usuario import Usuario
-
-usuario = Usuario(nome="João Silva", username="joaosilva", dataNascimento="1990-01-01", senha="senha123")
-UsuarioDAO.criar(usuario)
-
-print("Usuário cadastrado com sucesso!")
